14ec1fb5da0d8d3228b7774aab3138f280df0ade
Luna Andrade <doctor.diebo@gmail.com>
2023-03-17 22:14:19 +0000

    Add Post struct and start of scraping code

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,66 @@
-use std::thread;
-use std::time::Duration;
+use std::{path::Path, thread};
+use thirtyfour::{self, error::WebDriverResult, By, DesiredCapabilities, WebDriver};
+
+struct Post {
+    site: String,
+    author: String,
+    title: String,
+    content: String,
+}
 
 struct Crawler {
     found_nodes: Vec<String>,
+    found_posts: Vec<Post>,
+    driver: WebDriver,
 }
 
 impl Crawler {
-    fn init() -> Crawler {
-        Crawler {
+    async fn init() -> WebDriverResult<Crawler> {
+        let mut init_caps = DesiredCapabilities::firefox();
+        init_caps.set_firefox_binary(Path::new(
+            "/mnt/c/Users/Diego\\ Andrade/Desktop/Tor\\ Browser/Browser/firefox.exe",
+        ))?;
+
+        Ok(Crawler {
             found_nodes: vec![],
-        }
+            found_posts: vec![],
+            driver: WebDriver::new("http://localhost:4444", init_caps).await?,
+        })
     }
 
-    fn crawl(&self, source_nodes: Vec<&'static str>, depth: usize) -> () {
+    async fn scrape(&self, url: &str, class_name: &str) -> WebDriverResult<()> {
+        let mut posts: Vec<Post> = vec![];
+
+        self.driver.goto(url).await?;
+        self.driver.find(By::ClassName(class_name)).await?;
+
+        Ok(())
+    }
+
+    async fn crawl(&self, source_nodes: Vec<&'static str>, depth: usize) -> WebDriverResult<()> {
         if source_nodes.len() == 1 {
-            // crawl from 1
+            self.scrape(source_nodes[0], "postMain").await?;
         } else {
             // spawn threads and crawl
             let mut threads = vec![];
 
             for source in source_nodes {
-                threads.push(thread::spawn(move || {
-                    thread::sleep(Duration::from_secs(2));
-                    println!("{}", source);
-                }));
+                threads.push(thread::spawn(move || {}));
             }
 
             for thread in threads {
                 thread.join().unwrap();
             }
         }
+
+        Ok(())
     }
 }
 
-fn main() {
-    let crawler = Crawler::init();
-    crawler.crawl(vec!["test", "test1"], 1);
+#[tokio::main]
+async fn main() -> WebDriverResult<()> {
+    let crawler = Crawler::init().await?;
+    crawler.crawl(vec!["test", "test1"], 1).await?;
+
+    Ok(())
 }
